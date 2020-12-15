3a45ba10a0004945e9c65af0e4c3fb96037831b8
Meghan Carlson <mcarlson552@outlook.com>
2020-12-14 19:17:43 -0600

    updated the code

--- mad_libs.py
+++ mad_libs.py
@@ -1,34 +1,11 @@
-import sys
-import pygame
+# example
 
-class MadLibs:
-    """Overall class to manage the assets of the game and behavior."""
+noun = input("Enter a noun: ")
 
-    def __init__(self):
-        """Initialize the game and create game resources."""
-        pygame.init()
+verb = input("Enter a verb: ")
 
-        self.screen = pygame.display.set_mode((600, 600))
-        pygame.display.set_caption("MadLibs!")
+color = input("Enter a color: ")
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
-    
-    def run_game(self):
-        """Start the main loop for the game."""
-        while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.Quit:
-                    sys.exit()
-            
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+landmark = input("Enter a landmark: ")
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-    
-    if __name__ == '__main__':
-        # Make a game instance and run the game.
-        ai = MadLibs()
-        ai.run_game()
+print("One day we were " + verb + " past the " + landmark + " when a " + color + " " + noun + " flew right at us")
